0c012f1c8ecc834427d4800abb795aab995faeb9
jcg678 <jcg678@hotmail.es>
2016-06-06 13:20:30 +0200

    empezando javascript frontend

--- web/js/mapa.js
+++ web/js/mapa.js
@@ -1,10 +1,12 @@
-function initMap()
-{
-    console.log("prueba");
-    var jaen = {lat: 37.8139348, lng: -3.3807777};
+
+var latitud;
+var longitud;
+
+function crearMapa(punto,zoom){
+
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 9,
-        center: jaen,
+        zoom: zoom,
+        center: punto,
         streetViewControl: false,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         zoomControl: true,
@@ -18,3 +20,81 @@ function initMap()
     });
 
 }
+
+function initMap()
+{
+
+
+    function success(pos) {
+
+        var crd = pos.coords;
+         latitud= crd.latitude;
+        longitud=crd.longitude;
+
+        console.log('Your current position is:');
+        console.log('Latitude : ' + crd.latitude);
+        console.log('Longitude: ' + crd.longitude);
+        console.log('More or less ' + crd.accuracy + ' meters.');
+        var punto = {lat: latitud, lng: longitud};
+        zoom=14;
+
+        crearMapa(punto,zoom);
+
+        var marker = new google.maps.Marker({
+            position:punto,
+            map: map,
+            title: 'Hello World!'
+        });
+        marker.setIcon('https://dl.dropboxusercontent.com/u/20056281/Iconos/male-2.png');
+
+    };
+
+
+    function error(err) {
+        console.warn('ERROR(' + err.code + '): ' + err.message);
+    };
+    if (navigator.geolocation) {
+        punto = {lat: 37.8139348, lng: -3.3807777};
+        zoom = 9;
+        navigator.geolocation.getCurrentPosition(success, crearMapa(punto,zoom));
+    }
+    else
+    {
+        zoom=9;
+        console.log("XXx");
+        var punto = {lat: 37.8139348, lng: -3.3807777};
+        crearMapa(punto,zoom);
+    }
+
+    console.log("prueba");
+
+/*
+    var ajax_data = {
+        "id"     : blog.id,
+        "name"   : blog.name,
+        "url"    : blog.url,
+        "author" : blog.author
+    };
+
+    $.ajax({
+        url: destination.url,
+        data: ajax_data,
+        type: "post",
+        success: function(json) {
+
+        },
+        error:function (xhr, ajaxOptions, thrownError) {
+
+        }
+    });
+*/
+
+
+
+
+}
+
+
+
+
+
